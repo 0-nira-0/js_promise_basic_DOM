162fb17956e4212ea16726a7b691b7bae8a990c4
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-22 16:20:17 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
+const errorMessage = 'Promise was rejected!';
+const successfulMessage = 'Promise was resolved!';
+const messageClass = 'message';
+const errorMessageClasses = ['message', 'error-message'];
+
+function appendDiv(classList, message) {
+  const div = document.createElement('div');
+
+  div.classList.add(...classList);
+  div.textContent = message;
+
+  body.append(div);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(Error);
+  }, 3000);
+});
+
+promise1.then(appendDiv(messageClass, successfulMessage));
+promise2.catch(appendDiv(errorMessageClasses, errorMessage));
